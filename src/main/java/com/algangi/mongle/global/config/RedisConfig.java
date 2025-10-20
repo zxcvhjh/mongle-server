f0e378ee26f69492498972962e706dacc841596e
HJH <jijinny26@gmail.com>
2025-10-20 15:47:22 +0900

    feat: 게시물 세부 조회시 조회 정보 저장 + ttl 7일 갱신 lua script 추가

--- src/main/java/com/algangi/mongle/global/config/RedisConfig.java
+++ src/main/java/com/algangi/mongle/global/config/RedisConfig.java
@@ -72,4 +72,10 @@ public class RedisConfig {
         Resource scriptSource = new ClassPathResource("redis/get_reactions.lua");
         return RedisScript.of(scriptSource, List.class);
     }
+
+    @Bean
+    public RedisScript<Long> recordViewScript() {
+        Resource scriptSource = new ClassPathResource("redis/record_view.lua");
+        return RedisScript.of(scriptSource, Long.class);
+    }
 }
