e7a236e6f1bf86a6a9725d77eac54868868e5c1a
HJH <jijinny26@gmail.com>
2025-10-21 20:26:57 +0900

    fix: PostViewLog 중복 저장 시 오류 해결

--- src/main/java/com/algangi/mongle/postViewLog/domain/repository/PostViewLogRepository.java
+++ src/main/java/com/algangi/mongle/postViewLog/domain/repository/PostViewLogRepository.java
@@ -1,7 +1,5 @@
 package com.algangi.mongle.postViewLog.domain.repository;
 
-import com.algangi.mongle.member.domain.model.Member;
-import com.algangi.mongle.post.domain.model.Post;
 import com.algangi.mongle.postViewLog.domain.model.PostViewLog;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,4 +23,19 @@ public interface PostViewLogRepository extends JpaRepository<PostViewLog, String
 
     @EntityGraph(attributePaths = {"member", "post"})
     List<PostViewLog> findByMember_MemberIdAndCreatedDateAfter(String memberId, Instant since);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(
+            value = "INSERT INTO post_view_log (id, member_id, post_id, created_date) " +
+                    "VALUES (:id, :memberId, :postId, :createdDate) " +
+                    "ON DUPLICATE KEY UPDATE id = id",
+            nativeQuery = true
+    )
+    void insertIfNotExists(
+            @Param("id") String id,
+            @Param("memberId") String memberId,
+            @Param("postId") String postId,
+            @Param("createdDate") Instant createdDate
+    );
 }
