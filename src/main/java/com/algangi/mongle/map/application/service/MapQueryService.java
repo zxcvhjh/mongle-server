30bd8de024294fa60b3f8b7c8325bde00d64b97a
HJH <jijinny26@gmail.com>
2025-10-21 20:47:13 +0900

    feat: 지도 조회 시 익명화 기능 추가

--- src/main/java/com/algangi/mongle/map/application/service/MapQueryService.java
+++ src/main/java/com/algangi/mongle/map/application/service/MapQueryService.java
@@ -91,15 +91,24 @@ public class MapQueryService {
             .map(post -> {
                 Member author = authors.get(post.getAuthorId());
 
-                String profileImageUrl = null;
-                if (post.getStatus() == PostStatus.ACTIVE && author != null && author.getProfileImage() != null) {
-                    profileImageUrl = viewUrlIssueService.issueViewUrl(author.getProfileImage()).url();
-                }
+                boolean isAnonymous = post.getIsAnonymous();
+                MapObjectsResponse.Grain.Author authorDto;
 
-                MapObjectsResponse.Grain.Author authorDto = (author != null)
-                    ? new MapObjectsResponse.Grain.Author(author.getMemberId(),
-                    author.getNickname(), profileImageUrl)
-                    : new MapObjectsResponse.Grain.Author(null, "익명의 몽글러", null);
+                if (author == null) {
+                    authorDto = new MapObjectsResponse.Grain.Author(null, "익명의 몽글러", null);
+                } else if (isAnonymous) {
+                    authorDto = new MapObjectsResponse.Grain.Author(author.getMemberId(), "익명의 몽글러", null);
+                } else {
+                    String profileImageUrl = null;
+                    if (post.getStatus() == PostStatus.ACTIVE && author.getProfileImage() != null) {
+                        try {
+                            profileImageUrl = viewUrlIssueService.issueViewUrl(author.getProfileImage()).url();
+                        } catch (Exception e) {
+                            log.warn("Failed to issue view URL for profile image key in map query: {}", author.getProfileImage(), e);
+                        }
+                    }
+                    authorDto = new MapObjectsResponse.Grain.Author(author.getMemberId(), author.getNickname(), profileImageUrl);
+                }
 
                 boolean isViewed = viewedPostIds.contains(post.getId());
                 boolean isPostCreatedRecently = !post.getCreatedDate().isBefore(thirtyMinutesAgo);
