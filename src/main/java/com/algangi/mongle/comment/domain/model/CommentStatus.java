14bee8906bfc54f6e76258b04723551d8898b889
KIYEONG <rldud1237@naver.com>
2025-10-22 14:27:57 +0900

    feat(report): CommentStatus에 BLOCKED_BY_REPORTS 추가

--- src/main/java/com/algangi/mongle/comment/domain/model/CommentStatus.java
+++ src/main/java/com/algangi/mongle/comment/domain/model/CommentStatus.java
@@ -1,5 +1,5 @@
 package com.algangi.mongle.comment.domain.model;
 
 public enum CommentStatus {
-    ACTIVE, DELETED_BY_USER, DELETED_BY_ADMIN, DELETED_BY_WITHDRAWAL
+    ACTIVE, DELETED_BY_USER, DELETED_BY_ADMIN, DELETED_BY_WITHDRAWAL, BLOCKED_BY_REPORTS
 }
