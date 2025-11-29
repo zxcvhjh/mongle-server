0b41ea765fca7e2dc62c413f79088c8aa0437e1c
HJH <jijinny26@gmail.com>
2025-11-30 03:30:22 +0900

    feat(stats): Redis 통계 key용 StatsKeyPrefix enum 및 StatsKeyUtils 추가

---
+++ src/main/java/com/algangi/mongle/stats/domain/StatsKeyPrefix.java
@@ -0,0 +1,18 @@
+package com.algangi.mongle.stats.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum StatsKeyPrefix {
+    VIEWS("views"),
+    COMMENTS("comments"),
+    LIKES("likes"),
+    DISLIKES("dislikes"),
+    REACTIONS("reactions"),
+    COMMENT_RANKING_BY_LIKES("ranking:comments_by_likes:post"),
+    TRACKING("tracking");
+
+    private final String prefix;
+}
